f479728c018d94523f990a628efc16f191e01926
jorgepi <jpinomail@gmail.com>
2019-07-05 19:00:53 +0100

    Adding EKS master/workers and IAM roles/policies

---
+++ iam.tf
@@ -0,0 +1,69 @@
+# Role for the masters
+resource "aws_iam_role" "demo-eks-cluster" {
+  name = "terraform-eks-demo-eks-cluster"
+
+  assume_role_policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "eks.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+POLICY
+}
+
+resource "aws_iam_role_policy_attachment" "demo-eks-cluster-AmazonEKSClusterPolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+  role       = "${aws_iam_role.demo-eks-cluster.name}"
+}
+
+resource "aws_iam_role_policy_attachment" "demo-eks-cluster-AmazonEKSServicePolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSServicePolicy"
+  role       = "${aws_iam_role.demo-eks-cluster.name}"
+}
+
+# Role for the workers
+resource "aws_iam_role" "demo-eks-node" {
+  name = "terraform-eks-demo-node"
+
+  assume_role_policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "ec2.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+POLICY
+}
+
+resource "aws_iam_role_policy_attachment" "demo-eks-node-AmazonEKSWorkerNodePolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+  role       = "${aws_iam_role.demo-eks-node.name}"
+}
+
+resource "aws_iam_role_policy_attachment" "demo-eks-node-AmazonEKS_CNI_Policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+  role       = "${aws_iam_role.demo-eks-node.name}"
+}
+
+resource "aws_iam_role_policy_attachment" "demo-eks-node-AmazonEC2ContainerRegistryReadOnly" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+  role       = "${aws_iam_role.demo-eks-node.name}"
+}
+
+resource "aws_iam_instance_profile" "demo-eks-node" {
+  name = "terraform-eks-demo"
+  role = "${aws_iam_role.demo-eks-node.name}"
+}
